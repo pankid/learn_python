3b8154ba05f1447b6912226dbf8744fe193512a9
Nicholas Schoonover <pan@Nicholass-MacBook-Air.local>
2015-06-07 13:25:21 -0400

    learn python

---
+++ hi.py
@@ -0,0 +1,46 @@
+from random import randint
+
+board = []
+
+for x in range(0,5):
+	board.append(["W"] * 5)
+
+def print_board(board):
+	for row in board:
+		print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+	return randint(0, len(board) -1)
+
+def random_col(board):
+	return randint(0, len(board) -1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+guess_row = int(raw_input("Guess Row:"))
+guess_col = int(raw_input("Guess Col:"))
+
+
+print ship_row
+print ship_col
+
+win = 0
+
+while win == 0:
+	if guess_row == ship_row and guess_col == ship_col:
+		win = 1
+		print "Congratulations?"
+	
+	else:
+		if guess_row not in range(5) or guess_col not in range(5):
+			print "NAH"
+		elif board[guess_row][guess_col] == "X":
+			print "NOT AGAIN"
+		else:
+			board[guess_row][guess_col] = "X"
+			print "missed me" 
+			print_board(board)
+
